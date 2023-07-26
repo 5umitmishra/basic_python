c6d31cee19825d0367653d7d8b40ba4ff49859a6
Sumit Mishra <118822397+5umitmishra@users.noreply.github.com>
2023-07-26 23:06:28 +0530

    Add files via upload

---
+++ random number.py
@@ -0,0 +1,13 @@
+
+
+
+import random
+
+random_integers =random.randint(1 , 100)
+print(random_integers)
+
+
+random_float = random.random() *5
+print(random_float)
+
+love_score = random.randint(1,100)
