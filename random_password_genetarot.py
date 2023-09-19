3b01d63eb4fe2804dc81ba3f70a98d5e5287768e
Sumit Mishra <118822397+5umitmishra@users.noreply.github.com>
2023-09-19 20:09:29 +0530

    Update random_password_genetarot.py

--- random_password_genetarot.py
+++ random_password_genetarot.py
@@ -32,13 +32,3 @@ for char in password:
 print(f"your password is: {password_list}")
 
 
-
-
-
-
-#Eazy Level - Order not randomised:
-#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
-
-
-#Hard Level - Order of characters randomised:
-#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
