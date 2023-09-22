edb3d9b032030732d9872f8319fff850fd86a550
Sumit Mishra <118822397+5umitmishra@users.noreply.github.com>
2023-09-22 23:25:44 +0530

    Add files via upload

---
+++ Secret-Aucation-Program.py
@@ -0,0 +1,31 @@
+from replit import clear
+from art import logo
+
+# print(logo)
+print("Hii there, welcome to the secret aucation program")
+bidding = {}
+bidding_continue = True
+
+def highest_bidding(high_prise):
+    highest_bid = 0
+    winner = ""
+    for bidders in high_prise:
+        bid_amount = high_prise[bidders]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidders
+    print(f"the winner is {winner}, with the bid of {highest_bid}")
+
+
+while bidding_continue:
+    name = input("what is your name: ")
+    bidding_amount = int(input("whats your bidding amount: ₹ "))
+    bidding[name] = bidding_amount
+    bidders_in_bidding = input("is there any bidders? type 'Y' for yes and 'N' for no ")
+
+    if bidders_in_bidding == "n":
+        bidding_continue = False
+        highest_bidding(bidding)
+
+    elif bidders_in_bidding == "y":
+        clear()
