a9ef7510796c53ec7475f411365a2c2511f3a412
Sumit Mishra <118822397+5umitmishra@users.noreply.github.com>
2023-09-23 21:23:26 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,61 @@
+#calculator
+logo = """
+ _____________________
+|  _________________  |
+| | Pythonista   0. | |  .----------------.  .----------------.  .----------------.  .----------------. 
+| |_________________| | | .--------------. || .--------------. || .--------------. || .--------------. |
+|  ___ ___ ___   ___  | | |     ______   | || |      __      | || |   _____      | || |     ______   | |
+| | 7 | 8 | 9 | | + | | | |   .' ___  |  | || |     /  \     | || |  |_   _|     | || |   .' ___  |  | |
+| |___|___|___| |___| | | |  / .'   \_|  | || |    / /\ \    | || |    | |       | || |  / .'   \_|  | |
+| | 4 | 5 | 6 | | - | | | |  | |         | || |   / ____ \   | || |    | |   _   | || |  | |         | |
+| |___|___|___| |___| | | |  \ `.___.'\  | || | _/ /    \ \_ | || |   _| |__/ |  | || |  \ `.___.'\  | |
+| | 1 | 2 | 3 | | x | | | |   `._____.'  | || ||____|  |____|| || |  |________|  | || |   `._____.'  | |
+| |___|___|___| |___| | | |              | || |              | || |              | || |              | |
+| | . | 0 | = | | / | | | '--------------' || '--------------' || '--------------' || '--------------' |
+| |___|___|___| |___| |  '----------------'  '----------------'  '----------------'  '----------------' 
+|_____________________|
+"""
+print(logo)
+
+def add(n1, n2):
+    return n1 + n2
+
+def sub(n1, n2):
+    return n1 - n2
+
+def mul(n1, n2):
+    return n1 * n2
+
+def dev(n1 , n2):
+    return n1 / n2
+operations ={
+    "+":  add,
+    "-":  sub,
+    "*":  mul,
+    "/":  dev
+}
+def calcaluter():
+    num1 = float(input("pick a number: "))
+    should_continue = True
+    while should_continue:
+
+        num2 = float(input("pick a another number: "))
+        for things in operations:
+            print(things)
+
+
+        operations_symbol = input("pick an operations: ")
+
+        calcluting_value = operations[operations_symbol]
+        answer = calcluting_value(num1, num2)
+
+        print(f"{num1} {operations_symbol} {num2} = {answer} ")
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
+            num1 = answer
+        else:
+            should_continue = False
+            calcaluter()
+calcaluter()
+
+
+
